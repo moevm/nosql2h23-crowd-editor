3850c135f10e9e172c6201bd894dfb499b25e17a
23-58-20 <romacherepanov2002@gmail.com>
2023-12-20 00:42:00 +0300

    created stubs for a few nosql requests

---
+++ backend/dbms/dbms.py
@@ -0,0 +1,26 @@
+def get_users():
+    return []
+
+
+def get_books():
+    return []
+
+
+def add_user(user_desc: dict):
+    return True
+
+
+def add_book(book_desc: dict):
+    return True
+
+
+def get_user_pwd(login: str):
+    return ""
+
+
+def edit_user(user_desc: dict):
+    return True
+
+
+def edit_book(book_desc: dict):
+    return True
