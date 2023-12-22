5559fb74c11b3443413298f8d86a40a735ec0dc1
Deimos-Apollon <dima31120251@gmail.com>
2023-12-22 19:11:42 +0300

    fixed 500 when auth

--- backend/server/app.py
+++ backend/server/app.py
@@ -5,9 +5,8 @@ from backend.server import config
 
 app = Flask(__name__)
 
-
 @app.route("/users/all", methods=["GET"])
-def get_users():
+def get_users_all():
     error, users = dbms.get_users()
     if not error:
         return jsonify(users), 200
@@ -15,7 +14,7 @@ def get_users():
 
 
 @app.route("/books/all", methods=["GET"])
-def get_books():
+def get_books_all():
     error, books = dbms.get_books()
     if not error:
         return jsonify(books), 200
@@ -31,8 +30,8 @@ def get_users():
     return error, 500
 
 
-@app.route("/book", methods=["GET"])
-def get_books():
+@app.routes("/book", methods=["GET"])
+def get_book():
     filter_data = request.json
     error, books = dbms.filter_books(filter_data)
     if not error:
@@ -61,8 +60,8 @@ def edit_book():
 @app.route("/user/add", methods=["POST"])
 def add_user():
     add_data = request.json
-    check, users = dbms.add_user(add_data)
-    if check:
+    error_msg, users = dbms.add_user(add_data)
+    if not error_msg:
         return 200
     return 500
 
@@ -70,8 +69,8 @@ def add_user():
 @app.route("/book/add", methods=["POST"])
 def add_book():
     add_data = request.json
-    check, users = dbms.add_book(add_data)
-    if check:
+    error_msg, books = dbms.add_book(add_data)
+    if error_msg:
         return 200
     return 500
 
